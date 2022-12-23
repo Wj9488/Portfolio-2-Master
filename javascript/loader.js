d573e63f2a45fbd1a59dcc628fbd0576d09a727b
Will Jones <wtjones10@gmail.com>
2022-12-23 17:03:39 +0000

    Added gradient blur bg squares

--- javascript/loader.js
+++ javascript/loader.js
@@ -6,15 +6,16 @@ function preloader() {
     setInterval(() => {
         body.style.overflow = "visible";
     }, 0);
+    gsap.from(".hero__heading", {
+        opacity: 0, 
+        delay: 0,
+        duration: 0.5,
+        y: -10,
+    })
     gsap.from(".nav__container", {
         opacity: 0,
         duration: 0.5,
-    })
-    gsap.from(".hero__heading", {
-        opacity: 0, 
-        delay: 0.5,
-        duration: 0.5,
-        y: -10,
+        delay: 1.5,
     })
     gsap.to(".hero__divider", {
         delay: 1,
@@ -25,6 +26,11 @@ function preloader() {
         duration: 1,
         delay: 1.5,
         opacity: 0,
+    })
+    gsap.from(".bg__blur", {
+        duration: 4, 
+        opacity: 0, 
+        delay: 1.8,
     })
 }
 preloader()
